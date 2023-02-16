047a6d494808098f9ea4310dab7010875abcbabc
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2023-02-16 11:39:11 -0500

    Punto 1 y 7

---
+++ peoplePerCity.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "base_struct.h"
+
+unsigned int cityCount[9] = {0,0,0,0,0,0,0,0,0};
+
+void addPeoplePerCity(unsigned int cityNumber){
+	cityCount[cityNumber] = cityCount[cityNumber]+1;
+}
+const char * printPeoplePerCity(){
+	char arr[9][20] = {"Dallas", "New York City", "Los Angeles", "Mountain View", "Boston","Washington D.C.", "San Diego", "Austin", "Not found"};
+	static char peoplePerCity[400];
+	char str1[50], str2[50];
+	for (int i = 0; i < 9; i++)
+ 	{	
+ 		sprintf(str1,"Número de personas en %s", arr[i]);
+ 		sprintf(str2,": %d.\n",cityCount[i]);
+ 		strcat(peoplePerCity, str1);
+ 		strcat(peoplePerCity, str2);
+  	}	
+  	return peoplePerCity;
+}
