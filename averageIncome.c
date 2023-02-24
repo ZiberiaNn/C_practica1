72c89c40e47f9ee3817ea4cb8c7c7f31961c11e4
Kai-Al <Kai.albornoz@udea.edu.co>
2023-02-24 12:37:02 -0500

    Fix: En el punto 2, pide una ciudad en vez de imprimir todas.

--- averageIncome.c
+++ averageIncome.c
@@ -13,12 +13,19 @@ const char * printAverageIncome(item_t items[], int size){
     memset(averageIncome, 0, sizeof(averageIncome));
     int ageMinimum;
     int ageMaximum;
+    int cityAverageIncome;
     int cityCountPerAge[9] = {0,0,0,0,0,0,0,0,0};
     // Pide la edad minima y maxima por consola
     printf("Ingrese la edad minima: ");
     scanf("%d", &ageMinimum);
     printf("Ingrese la edad maxima: ");
     scanf("%d", &ageMaximum);
+    printf("Ingrese el número correspondiente a la ciudad:\n0 = Dallas, 1 = New York City, 2 = Los Angeles, 3 = Mountain View, 4 = Boston, 5 = Washington D.C., 6 = San Diego, 7 = Austin.\n* ");
+    scanf("%d",&cityAverageIncome);
+
+    if(cityAverageIncome>8){
+        cityAverageIncome = 8;
+    }
 
     // Valida que la edad del elemento se encuentra entre la edad minima y maxima
     // Y suma el ingreso de la persona a la ciudad correspondiente
@@ -36,20 +43,17 @@ const char * printAverageIncome(item_t items[], int size){
     //String auxiliar que sirve para concatenar cada linea en el string del resultado general
     char str[100];
 
-    for (int i = 0; i < 9; i++)
-    {	  
-        if(cityCountPerAge[i] == 0){
-            //Guarda el resultado i en str
-            snprintf(str, sizeof(str), "No hay personas en %s en el rango de edad ingresado.\n", city_names[i]);
-            //Concatena str con el resultado general
-            strcat(averageIncomePerCity, str);
-            continue;
-        }
+    int i = cityAverageIncome;
+    if(cityCountPerAge[i] == 0){
         //Guarda el resultado i en str
-        averageIncome[i] = averageIncome[i]/cityCountPerAge[i]; 
-        snprintf(str, sizeof(str), "Promedio de ingresos en %s: %d.\n", city_names[i], averageIncome[i]);
+        snprintf(str, sizeof(str), "No hay personas en %s en el rango de edad ingresado.\n", city_names[i]);
         //Concatena str con el resultado general
         strcat(averageIncomePerCity, str);
     }
+    //Guarda el resultado i en str
+    averageIncome[i] = averageIncome[i]/cityCountPerAge[i]; 
+    snprintf(str, sizeof(str), "Promedio de ingresos en %s: %d.\n", city_names[i], averageIncome[i]);
+    //Concatena str con el resultado general
+    strcat(averageIncomePerCity, str);
     return averageIncomePerCity;
 }
